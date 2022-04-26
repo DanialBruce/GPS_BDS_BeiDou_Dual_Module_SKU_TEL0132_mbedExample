bc2fd8c3b1e3b9928d60691b1a0909863b8025cc
DanialBruce <54834585+DanialBruce@users.noreply.github.com>
2022-04-27 01:58:19 +0300

    mqtt and Ethernet code added. Libraries are still missing

--- main.cpp
+++ main.cpp
@@ -1,10 +1,51 @@
 #include "mbed.h"
+#include "MQTTClientMbedOs.h"
+#include "EthernetInterface.h"
+#include <chrono>
 #include <cstdio>
 #include <cstring>
+#include <string>
+
+// Maximum number of element the application buffer can contain
+#define MAXIMUM_BUFFER_SIZE                                                  128
+
+// threads
+
+Thread publishThread;
+
+//network, socket and mqtt
+EthernetInterface eth;
+TCPSocket socket;
+MQTTClient client(&socket);
+MQTTPacket_connectData mqttData;
+
+// ip addresses
+SocketAddress MQTTBrokerAddress;
+SocketAddress DeviceAddress;
+SocketAddress BuffAddr;
 
 
-// code is based on Arduino example, found in: https://wiki.dfrobot.com/GPS_+_BDS_BeiDou_Dual_Module_SKU_TEL0132
+DigitalOut dataReadyLED(A0);
+DigitalIn gpsPosReady(D7, PullDown);    // PPS pin on GPS module. Can be used to create data synchronization, interrupts etc.
+DigitalOut errorLED(D8);
 
+
+// Create a BufferedSerial object with a default baud rate.
+static BufferedSerial GpsSerial(PTC17, PTC16);
+
+
+
+//functions
+void eth_connection_init();
+void connectToBroker();
+void PublishMsg(char* topic, char* buf);
+void PublishHandler();
+void Error_Flag(int num);
+void print_GpsDATA();
+void parse_GpsDATA();
+void Read_Gps();
+void gpsFormatter(char *rawLat, char *rawLong);
+void RST_GpsRxBuffer(void);
 
 struct
 {
@@ -20,44 +61,125 @@ struct
 } Save_Data;
 
 
-DigitalOut errorLED(D8);
-DigitalIn gpsPosReady(D7, PullDown);
 
+
+//  buffers and related vars
 const unsigned int gpsRxBufferLength = 1024;
 char gpsRxBuffer[gpsRxBufferLength] = {0};
 char tmpBuf[4] = {0};
+char msg[256];
+
+char jsonStrForm [] = "{\"gps\":{\"data\":\"%s\", \"UTCtime\":%s, \"latitude\":%s, \"N_S\":\"%s\", \"longitude\":%s, \"E_W\":\"%s\"}}";
+
+
 
 unsigned int gpsRxLength = 0;
 uint32_t num;
 
-static UnbufferedSerial GpsSerial(PTC17, PTC16);
-
-//functions
-void Error_Flag(int num);
-void print_GpsDATA();
-void parse_GpsDATA();
-void Read_Gps();
-void RST_GpsRxBuffer(void);
-
-
 // main() runs in its own thread in the OS
 int main()
 {
-    printf("DFRobot Gps\n");
-    printf("Wating...\n");
+    printf("Program started\n");
+    // Set desired properties (9600-8-N-1).
+    GpsSerial.set_baud(9600);
+    GpsSerial.set_format(
+        /* bits */ 8,
+        /* parity */ BufferedSerial::None,
+        /* stop bit */ 1
+    );
+    ThisThread::sleep_for(1s);
+
+    // test the LED
+    dataReadyLED.write(1);
+    ThisThread::sleep_for(500ms);
+    //set to zero at start
+    dataReadyLED.write(0);
     
+    //Network and MQTT setup
+    eth_connection_init();
+    connectToBroker();
+    printf("<<< Network and connection setup is complete >>>\n\r");
+    ThisThread::sleep_for(1s);
+
+
     Save_Data.GetData_Flag = false;
     Save_Data.ParseData_Flag = false;
     Save_Data.Usefull_Flag = false;
 
+    printf("DFRobot Gps\n");
+    printf("Wating...\n");
+    Save_Data.GetData_Flag = false;
+    Save_Data.ParseData_Flag = false;
+    Save_Data.Usefull_Flag = false;
+    
+    publishThread.start(PublishHandler);
 
     while (true) {
         Read_Gps();         //Get GPS data 
         parse_GpsDATA();    //Analyze GPS data 
-        print_GpsDATA();    //Output analyzed data 
+        print_GpsDATA();    //Output analyzed data
     }
+    printf("Existed back to main\n"); //normally, it should not ever reach here
+
+}
+void eth_connection_init(){
+        // Bring up the ethernet interface and connect to network
+    eth.connect();
+    
+    //gather network address, netmask and gateway address and show them
+    eth.get_ip_address(&DeviceAddress);
+    ThisThread::sleep_for(1s);
+
+    printf("IP address: %s\n", DeviceAddress.get_ip_address() ? DeviceAddress.get_ip_address() : "None");
+
+    eth.get_gateway(&BuffAddr);
+    printf("Gateway address: %s\n", BuffAddr.get_ip_address() ?BuffAddr.get_ip_address() : "None");
+    
+    eth.get_netmask(&BuffAddr);
+    printf("Gateway address: %s\n",  BuffAddr.get_ip_address() ? BuffAddr.get_ip_address() : "None");
+
+    // Tell the TCP socket to use the instantiated Ehternet Interface
+    socket.open(&eth);
+    ThisThread::sleep_for(2s);
 }
 
+void connectToBroker(){
+    //get the ip address of remote broker host address
+    eth.gethostbyname(MBED_CONF_APP_MQTT_BROKER_HOSTNAME, &MQTTBrokerAddress, NSAPI_IPv4, "myEthernet");
+    //set port to Broker IP address to be used in MQTTClient 
+    MQTTBrokerAddress.set_port(MBED_CONF_APP_MQTT_BROKER_PORT);
+    socket.connect(MQTTBrokerAddress);
+
+    //print the Translated IP address of MQTT host Broker
+    printf("MQTT host Broker address: %s\n",  MQTTBrokerAddress.get_ip_address() ? MQTTBrokerAddress.get_ip_address() : "None");
+
+    // configuring the settings and options for connecting to broker
+    mqttData = MQTTPacket_connectData_initializer;
+        // This MQTTClient ID/name
+    mqttData.clientID.cstring = MBED_CONF_APP_MQTT_CLIENT_ID;
+    
+    client.connect(mqttData);
+}
+
+void PublishMsg(char* topic, char* buf){
+    MQTT::Message msg;
+    msg.qos = MQTT::QOS0;
+    msg.retained = false;
+    msg.dup = false;
+    msg.payload = (void*)buf;
+    msg.payloadlen = strlen(buf);
+    client.publish(topic, msg);
+    client.yield(250);
+}
+
+void PublishHandler(){
+    while (true) {
+            if (Save_Data.Usefull_Flag) {
+                sprintf(msg, jsonStrForm, Save_Data.GPS_DATA, Save_Data.UTCTime, Save_Data.latitude, Save_Data.N_S, Save_Data.longitude, Save_Data.E_W);
+                PublishMsg(MBED_CONF_APP_MQTT_TOPIC, msg);
+            }
+    }
+}
 
 void Error_Flag(int num){
     // printf("Entered Error_Flag(int num)\n");
@@ -80,11 +202,12 @@ void print_GpsDATA(){
 
         if(Save_Data.Usefull_Flag)
         {
+            gpsFormatter(Save_Data.latitude, Save_Data.longitude);
             Save_Data.Usefull_Flag = false;
             printf("Save_Data.latitude = %s\n", Save_Data.latitude);
 
             printf("Save_Data.N_S = %s\n", Save_Data.N_S);
-            
+
             printf("Save_Data.longitude = %s\n", Save_Data.longitude);
 
             printf("Save_Data.E_W = %s\n", Save_Data.E_W);
@@ -130,6 +253,7 @@ void parse_GpsDATA(){
             case 6:memcpy(Save_Data.E_W, subString, subStringNext - subString);break;        //Get E/W
 
             default:break;
+
           }
           subString = subStringNext;
           Save_Data.ParseData_Flag = true;
@@ -138,6 +262,7 @@ void parse_GpsDATA(){
           else if(usefullBuffer[0] == 'V')
             Save_Data.Usefull_Flag = false;
         }
+        
         else
         {
           Error_Flag(2);    //Analysis error
@@ -181,4 +306,52 @@ void RST_GpsRxBuffer(void){
 
     memset(gpsRxBuffer, 0, gpsRxBufferLength - 20);
     gpsRxLength = 0;
+}
+
+void gpsFormatter(char* rawLat, char* rawLong){
+    char buf[12];
+    int intLat;
+    int intLong;
+
+    float fLat;
+    float fLong;
+    int i;
+
+    // lat. format conversion
+    for (i = 0; i < 2; i++) {
+        buf[i] = rawLat[i];
+    }
+
+    intLat = atoi(buf);
+    memset(buf, 0, 12);
+
+    for (; i < strlen(rawLat); i++) {
+        buf[i - 2] = rawLat[i];
+    }
+    fLat = atof(buf);
+    memset(buf, 0, 12);
+
+    // long. format conversion
+
+    for (i = 0; i < 3; i++) {
+        buf[i] = rawLong[i];
+    }
+
+    intLong = atoi(buf);
+    memset(buf, 0, 12);
+
+    for (; i < strlen(rawLong); i++) {
+        buf[i - 3] = rawLong[i];
+    }
+    fLong = atof(buf);
+    memset(buf, 0, 12);
+
+    fLat = intLat + (fLat / 60);
+    fLong = intLong + (fLong / 60);
+    
+    memset(Save_Data.latitude, 0, 11);
+    memset(Save_Data.longitude, 0, 12);
+
+    sprintf(Save_Data.latitude, "%f", fLat);
+    sprintf(Save_Data.longitude, "%f", fLong);
 }
